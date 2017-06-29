4f5e9e166d0dd61661ad533d42a24628dbe547fa
douglasmg7 <douglasmg7@gmail.com>
2017-06-29 10:42:42 -0300

    store item prototype

--- webpack/storeItem.vue
+++ webpack/storeItem.vue
@@ -1,45 +1,87 @@
 <template lang='pug'>
   div
-    .ui.black.inverted.borderless.attached.stackable.menu
-      // Home.
-      a.ui.link.item(href='/')
-        h2 Zunka
-      .ui.right.inverted.borderless.stackable.menu
-        // Search.
-        .ui.item
-          .ui.small.icon.input
-            input(v-model='search' v-on:keyup.enter='getProducts()' placeholder='O que você procura?' type='text' size='40')
-            i.search.link.icon(v-on:click='getProducts()')
-        // User name.
-        .ui.item
-          i.large.user.icon(v-if="this.user.username") 
-          | {{this.user.username}}
-        // Sign-in.
-        a.ui.item(href='/users/login' v-if="!this.user.username")
-          i.large.icon.sign.in
-          | Entrar
-        // Cart.
-        a.ui.item
-          i.large.cart.icon
-        // Config.
-        a.ui.item(href='/configProducts/store' v-if="this.user.group == 'admin'")
-          i.large.configure.icon
-        // Exit.
-        a.ui.item(href='/users/logout' v-if="this.user.username")
-          i.large.sign.out.icon
-          | Sair   
-    .ui.center.aligned.container
-      .ui.basic.padded.segment
-        .ui.item
+    nav.navbar.navbar-inverse.navbar-static-top
+      //- .container-fluid
+      .container
+        .navbar-header
+          button.navbar-toggle.collapsed(type='button' data-toggle='collapse' data-target='#navbar' aria-expanded='false' aria-controls='navbar')
+            span.sr-only Toggle navigation
+            span.icon-bar
+            span.icon-bar
+            span.icon-bar     
+          // Home.
+          a.navbar-brand(href='/') ZUNKA
+        #navbar.navbar-collapse.collapse
+          ul.nav.navbar-nav.navbar-right
+            li
+              // Username.
+              a(v-if="this.user.username") {{this.user.username}}
+            li
+              // Sign-in.
+              a(href='/users/login' v-if="!this.user.username") Entrar
+            li
+              // Cart.
+              a Carrinho
+            li
+              // Config.
+              a(href='/configProducts/store' v-if="this.user.group == 'admin'") Config
+            li
+              // Exit.
+              a(href='/users/logout' v-if="this.user.username") Sair
+          form.navbar-form.navbar-right
+            input.form-control(v-model='search' v-on:keyup.enter='getProducts()' placeholder='O que você procura?' type='text' size='40')
+    .container
+      .row
+        .col-md-2
+        .col-md-8
           .image
             img(:src='imgUrl()')
           .content
-            .header {{product.storeProductTitle}}
-            .description {{product.storeProductDescPrimary}}
-            .price
+            // Title.
+            h3 Raspberry PI 3 Model B A1.2GHz 64-bit quad-core ARMv8 CPU, 1GB RAM
+            // Price
+            h2.product-price
               sup R$
               | {{product.storeProductPrice | currencyInt}}
               sup {{product.storeProductPrice | currencyCents}}
+            // List of caracteristics.
+            .product-detail
+              ul
+                li 1.2GHz 64-bit quad-core ARMv8 CPU, 1 GB RAM
+                li 802.11n Wireless LAN, 10/100Mbps Lan Speed 
+                li Bluetooth 4.1, Bluetooth Low Energy 
+                li 4 USB ports, 40 GPIO pins, Full HDMI port, Combined 3.5mm audio jack and composite video 
+                li Camera interface (CSI),Display interface (DSI), Micro SD card slot (now push-pull rather than push-push), VideoCore IV 3D graphics core
+            // Complete description.
+            .product-description
+              h3 Descricao do produto
+              p Built on the latest Broadcom 2837 ARMv8 64 bit processor the Raspberry Pi 3 Model B is faster and more powerful than its predecessors. It has improved power management to support more powerful external USB devices and now comes with built-in wireless and Bluetooth connectivity. To take full advantage of the improved power management on the Raspberry Pi 3 and provide support for even more powerful devices on the USB ports, a 2.5A adapter is required. Technical Specifications: - Broadcom BCM2837 64bit ARMv8 QUAD Core 64bit Processor powered Single Board Computer running at 1.2GHz - 1GB RAM - BCM43143 WiFi on board - Bluetooth Low Energy (BLE) on board - 40pin extended GPIO - 4 x USB2 ports - 4 pole Stereo output and Composite video port - Full size HDMI - CSI camera port for connecting the Raspberry Pi camera - DSI display port for connecting the Raspberry Pi touch screen display - MicroSD port for loading your operating system and storing data - Upgraded switched Micro USB power source (now supports up to 2.5 Amps) This product is made under license in both China and the U.K. Please see the product packaging.
+            // Table of information.
+            .product-information
+              h3 Informacao do produto
+              .techincal
+                h4 Detalhe tecnico
+                .table-responsive
+                  table.table.table-striped
+                    tbody
+                      tr
+                        td Screen Size 
+                        td 60 inches 
+                      tr
+                        td Processor 
+                        td 1.2 GHz 
+              .additional
+                h4 Informacoes adicionais
+                .table-responsive
+                  table.table.table-striped
+                    tbody
+                      tr
+                        td Brand Name 
+                        td Raspberry Pi 
+                      tr
+                        td Series 
+                        td RASPBERRYPI3-MODB-1GB
+        .col-md-2
 </template>
 <script>
   /* globals accounting */
