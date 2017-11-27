74b6e35eeb934e8e9bee0c5a9d1fd70e81c2546d
douglasmg7 <douglasmg7@gmail.com>
2017-11-27 10:10:29 -0200

    creating menu

--- webpack/menuProducts.vue
+++ webpack/menuProducts.vue
@@ -1,20 +1,18 @@
 <template lang='pug'>
-  ul
-    li teste
-    li 
-      p teste2
-    // Home.
-    li: a(href='/'): h4 Site
-    // Store.
-    li: a(href='store' v-bind:class='{"active": active === "store"}'): h4 Zunka
-    // Allnations.
-    li: a(href='allnations' v-bind:class='{"active": active === "allNations"}'): h4 All Nations
-    // Create product.
-    //- div(v-if='active == "store"')
+  .menu
+    ul
+      // Home.
+      li: a(href='/'): h4 Site
+      // Store.
+      li: a(href='store' v-bind:class='{"active": active === "store"}'): h4 Zunka
+      // Allnations.
+      li: a(href='allnations' v-bind:class='{"active": active === "allNations"}'): h4 All Nations
+      // Create product.
+    .space
+    .search(v-if='active == "store"')
       button.ui.button(v-on:click='$emit("newProduct")') Criar Produto
       input(v-on:input='$emit("input", $event.target.value)' v-on:keypress='emitSearch($event)' v-model='searchText' placeholder='Pesquisa')
       i.search.link.icon(v-on:click='$emit("search")')
-    //- div
       // User name.
       a(href='/users/account' v-if="this.user.username"): i {{this.user.username}}
       // Sign-in.
@@ -51,13 +49,31 @@
 </script>
 
 <style lang='stylus'>
+  html, body, div, form, p
+    margin: 0
+    padding: 0
+  .menu h4
+    margin: .5em
+  .menu a
+    text-decoration: none
   ul
+    display: inline-block
     list-style-type: none
     margin: 0
     padding: 0
-    border: 1px solid
+    border: 1px solid green
   li
-    display: block-inline
-    width: 80px
+    display: inline-block
     border: 1px solid red
+    padding: 0
+  .space
+    display: inline-block
+    border: 1px solid yellow
+  .search
+    float: right
+    border: 1px solid brown
+  .user
+    display: inline-block
+    border: 1px solid brown
+
 </style>
