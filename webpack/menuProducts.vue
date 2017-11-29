48c6d372ba9b936e8c4c5fa69ec92765b8d9e8be
douglasmg7 <douglasmg7@gmail.com>
2017-11-29 11:39:56 -0200

    menu on ul

--- webpack/menuProducts.vue
+++ webpack/menuProducts.vue
@@ -4,21 +4,22 @@
       // Home.
       li: a(href='/') Site
       // Store.
-      li: a(href='store' v-bind:class='{"active": active === "store"}') h4
+      li: a(href='store' v-bind:class='{"active": active === "store"}') Loja
       // Allnations.
-      li: a(href='allnations' v-bind:class='{"active": active === "allNations"}') All Nations
-    // Create product.
-    button.ui.button(v-on:click='$emit("newProduct")') Criar Produto
-    .test
-    //- .search(v-if='active == "store"')
-      input(v-on:input='$emit("input", $event.target.value)' v-on:keypress='emitSearch($event)' v-model='searchText' placeholder='Pesquisa')
-      i.search.link.icon(v-on:click='$emit("search")')
-      // User name.
-      a(href='/users/account' v-if="this.user.username"): i {{this.user.username}}
-      // Sign-in.
-      a(href='/users/login' v-if="!this.user.username"): i Entrar
-      // Exit.
-      a(href='/users/logout' v-if="this.user.username"): i Sair
+      li: a(href='allnations' v-bind:class='{"active": active === "allNations"}') All Nations      
+      li: button.create-product Criar produto
+
+      
+    //- button.search(v-on:click='$emit("newProduct")') Criar Produto
+  //- .search(v-if='active == "store"')
+    input(v-on:input='$emit("input", $event.target.value)' v-on:keypress='emitSearch($event)' v-model='searchText' placeholder='Pesquisa')
+    i.search.link.icon(v-on:click='$emit("search")')
+    // User name.
+    a(href='/users/account' v-if="this.user.username"): i {{this.user.username}}
+    // Sign-in.
+    a(href='/users/login' v-if="!this.user.username"): i Entrar
+    // Exit.
+    a(href='/users/logout' v-if="this.user.username"): i Sair
 </template>
 
 <script>
@@ -49,42 +50,26 @@
 </script>
 
 <style lang='stylus'>
-  html, body, div, form, p
+  html, body, div, form, ul, li
     margin: 0
     padding: 0
   .menu
     background-color: black
-    border: 4px solid green
   .menu a
-    font-size: 3.5em
+    font-size: 1.2em
     color: #ccc
     text-decoration: none
-    padding: .5em
   .menu a:hover
     color: #fff
-  .menu button
-    margin: auto
+  button.create-product
+    margin: 0
     padding: 0
-    font-size: 1em
-    // display: inline-block
-    // vertical-align: middle
-    position: relative
-    // top: 50%
-    // transform: translateY(-10%)
-    color: red
-  .test
-    display: inline-block
-    width: 2em
-    height: 2em
-    background-color: blue
+    font-size: 1em  
   ul
     display: inline-block
     list-style-type: none
-    margin: 0
-    padding: 0
-    border: 4px solid yellow
   li
     display: inline-block
-    border: 4px solid red
-    padding: 0
+    margin: .4em 1em .4em 1em
+    // border: 4px solid red
 </style>
