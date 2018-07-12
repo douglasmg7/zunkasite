2f53ca1dffe7979a5cc5973c09044a88dd2cf613
douglasmg7 <douglasmg7@gmail.com>
2018-07-12 16:31:46 -0300

    saving edit address not working

--- dist/js/user/editAddress.js
+++ dist/js/user/editAddress.js
@@ -18,6 +18,23 @@ var app = new Vue({
     address: address
   },
   methods: {
+    getPrdouctAddedToCart(){
+      let regExpResult = /[?&]productAddedToCart=([^&#]*)/.exec(window.location.href);
+      if (regExpResult) {
+        let product_id = regExpResult[1];
+        axios({
+          method: 'get',
+          url: `/api/product/${product_id}`,
+          headers:{'csrf-token' : csrfToken}
+        })
+        .then((res)=>{
+          this.productAddedToCart = res.data.product;
+        })
+        .catch((err)=>{
+          console.error(`Error - getProducts(), err: ${err}`);
+        });
+      }
+    },
     // Set default address.
     saveAddress(addressId){
       // Verify CEP.
@@ -31,28 +48,33 @@ var app = new Vue({
       this.address.cep = this.address.cep.replace(/\D/g, '');
       // Format 00000-000
       this.address.cep = this.address.cep.match(/^\d{5}/)[0] + '-' + this.address.cep.match(/\d{3}$/)[0];
-      axios({
-        method: 'post',
-        url: `/user/address/edit`,
-        headers: {'csrf-token' : csrfToken},
-        data: { 
-          address: this.address
-        }
-      })
-      .then((res)=>{
-        // Successful set address as default.
-        if (res.data.success) {
-          window.location.href = '/user/address';
-        }
-        // Something wrong.
-        else
-        {
-          this.warnMessage = res.data.message;
-        }
-      })
-      .catch((err)=>{
-        console.error(`Error - saveAddress(), err: ${err}`);
-      });
+      // Get addressId from url query.
+      let regExpResult = /[?&]addressId=([^&#]*)/.exec(window.location.href);
+      console.log(`regExp: ${regExpResult[1]}`);
+      if (regExpResult) {
+        axios({
+          method: 'post',
+          url: `/user/address/edit`,
+          // url: `/user/address/edit?addressId=${regExpResult[1]}`,
+          headers: {'csrf-token' : csrfToken},
+          data: { address: this.address },
+          params: { addressId: regExpResult[1] }
+        })
+        .then((res)=>{
+          // Successful set address as default.
+          if (res.data.success) {
+            window.location.href = '/user/address';
+          }
+          // Something wrong.
+          else
+          {
+            this.warnMessage = res.data.message;
+          }
+        })
+        .catch((err)=>{
+          console.error(`Error - saveAddress(), err: ${err}`);
+        });
+      }
     }
   },
   watch: {
--- routes/routeUser.js
+++ routes/routeUser.js
@@ -580,19 +580,20 @@ router.get('/address/edit', (req, res, next)=>{
 });
 
 // Edit address.
-router.post('/address/add', checkPermission, (req, res, next)=>{
-  log.debug(`query: ${req.query.addressId}`)
-  // log.debug(`body: ${JSON.stringify(req.body)}`)
+router.post('/address/edit', checkPermission, (req, res, next)=>{
+  log.debug('inside address edit.');
+  log.debug(`query: ${req.query.addressId}`);
+  log.debug(`body: ${JSON.stringify(req.body)}`);
   // Validation.
-  req.checkBody('name', 'Campo NOME deve ser preenchido.').notEmpty();
-  req.checkBody('cep', 'Campo CEP deve ser preenchido.').notEmpty();
-  req.checkBody('cep', 'CEP inválido.').isCep();
-  req.checkBody('address', 'Campo ENDEREÇO deve ser preenchido.').notEmpty();
-  req.checkBody('addressNumber', 'Campo NÚMERO deve ser preenchido.').notEmpty();
-  req.checkBody('district', 'Campo BAIRRO deve ser preenchido.').notEmpty();
-  req.checkBody('city', 'Campo CIDADE deve ser preenchido.').notEmpty();
-  req.checkBody('state', 'Campo ESTADO deve ser preenchido.').notEmpty();
-  req.checkBody('phone', 'Campo TELEFONE deve ser preenchido.').notEmpty();
+  req.checkBody('address.name', 'Campo NOME deve ser preenchido.').notEmpty();
+  req.checkBody('address.cep', 'Campo CEP deve ser preenchido.').notEmpty();
+  req.checkBody('address.cep', 'CEP inválido.').isCep();
+  req.checkBody('address.address', 'Campo ENDEREÇO deve ser preenchido.').notEmpty();
+  req.checkBody('address.addressNumber', 'Campo NÚMERO deve ser preenchido.').notEmpty();
+  req.checkBody('address.district', 'Campo BAIRRO deve ser preenchido.').notEmpty();
+  req.checkBody('address.city', 'Campo CIDADE deve ser preenchido.').notEmpty();
+  req.checkBody('address.state', 'Campo ESTADO deve ser preenchido.').notEmpty();
+  req.checkBody('address.phone', 'Campo TELEFONE deve ser preenchido.').notEmpty();
   req.getValidationResult().then(function(result) {
     // Send validations errors to client.
     if (!result.isEmpty()) {
@@ -600,8 +601,8 @@ router.post('/address/add', checkPermission, (req, res, next)=>{
       messages.push(result.array()[0].msg);
       return res.json({ success: false, message: messages[0]});
     } 
-    // Save address.
-    else {
+    // Save address new address.
+    else if (req.query.addressId === 'new'){
       let address = new Address();
       address.user_id = req.user._id;
       address.name = req.body.name;
@@ -618,6 +619,27 @@ router.post('/address/add', checkPermission, (req, res, next)=>{
         return res.json({ success: true });
       });        
     }
+    // save specific address.
+    else if (req.query.addressId) {
+      Address.findByIdAndUpdate(req.query.addressId, { 
+        $set: { 
+          name: req.body.name;
+          cep: req.body.cep;
+          address: req.body.address;
+          addressNumber: req.body.addressNumber;
+          addressComplement: req.body.addressComplement;
+          district: req.body.district;
+          city: req.body.city;
+          state: req.body.state;
+          phone: req.body.phone;
+        }}, err=>{
+          if (err) return next(err);
+      });
+    }
+    // Not new no edit addres.
+    else{
+      return res.json({ success: false, message: 'Erro interno do sistema.' });
+    }
   });
 });
 
@@ -631,50 +653,50 @@ router.post('/address/add', checkPermission, (req, res, next)=>{
 //   });
 // });
 
-// Edit address.
-router.post('/address/edit', checkPermission, (req, res, next)=>{
-  // Validation.
-  req.checkBody('name', 'Campo NOME deve ser preenchido.').notEmpty();
-  req.checkBody('cep', 'Campo CEP deve ser preenchido.').notEmpty();
-  req.checkBody('address', 'Campo ENDEREÇO deve ser preenchido.').notEmpty();
-  req.checkBody('addressNumber', 'Campo NÚMERO deve ser preenchido.').notEmpty();
-  req.checkBody('district', 'Campo BAIRRO deve ser preenchido.').notEmpty();
-  req.checkBody('city', 'Campo CIDADE deve ser preenchido.').notEmpty();
-  req.checkBody('state', 'Campo ESTADO deve ser preenchido.').notEmpty();
-  req.checkBody('phone', 'Campo TELEFONE deve ser preenchido.').notEmpty();
-  req.getValidationResult().then(function(result) {
-    // Send validations errors to client.
-    if (!result.isEmpty()) {
-      let messages = [];
-      messages.push(result.array()[0].msg);
-      req.flash('error', messages);
-      res.redirect('back');
-      return;
-    } 
-    // Save address.
-    else {
-      if (!req.body.addressId) { return next(new Error('No addressId to find address data.')); }
-      Address.findById(req.body.addressId, (err, address)=>{
-        if (err) { return next(err) };
-        if (!address) { return next(new Error('Not found address to save.')); }
-        address.user_id = req.user._id;
-        address.name = req.body.name;
-        address.cep = req.body.cep;
-        address.address = req.body.address;
-        address.addressNumber = req.body.addressNumber;
-        address.addressComplement = req.body.addressComplement;
-        address.district = req.body.district;
-        address.city = req.body.city;
-        address.state = req.body.state;
-        address.phone = req.body.phone;
-        address.save(function(err) {
-          if (err) { return next(err); } 
-          res.redirect('/user/address');
-        });  
-      });
-    }
-  });
-});
+// // Edit address.
+// router.post('/address/edit', checkPermission, (req, res, next)=>{
+//   // Validation.
+//   req.checkBody('name', 'Campo NOME deve ser preenchido.').notEmpty();
+//   req.checkBody('cep', 'Campo CEP deve ser preenchido.').notEmpty();
+//   req.checkBody('address', 'Campo ENDEREÇO deve ser preenchido.').notEmpty();
+//   req.checkBody('addressNumber', 'Campo NÚMERO deve ser preenchido.').notEmpty();
+//   req.checkBody('district', 'Campo BAIRRO deve ser preenchido.').notEmpty();
+//   req.checkBody('city', 'Campo CIDADE deve ser preenchido.').notEmpty();
+//   req.checkBody('state', 'Campo ESTADO deve ser preenchido.').notEmpty();
+//   req.checkBody('phone', 'Campo TELEFONE deve ser preenchido.').notEmpty();
+//   req.getValidationResult().then(function(result) {
+//     // Send validations errors to client.
+//     if (!result.isEmpty()) {
+//       let messages = [];
+//       messages.push(result.array()[0].msg);
+//       req.flash('error', messages);
+//       res.redirect('back');
+//       return;
+//     } 
+//     // Save address.
+//     else {
+//       if (!req.body.addressId) { return next(new Error('No addressId to find address data.')); }
+//       Address.findById(req.body.addressId, (err, address)=>{
+//         if (err) { return next(err) };
+//         if (!address) { return next(new Error('Not found address to save.')); }
+//         address.user_id = req.user._id;
+//         address.name = req.body.name;
+//         address.cep = req.body.cep;
+//         address.address = req.body.address;
+//         address.addressNumber = req.body.addressNumber;
+//         address.addressComplement = req.body.addressComplement;
+//         address.district = req.body.district;
+//         address.city = req.body.city;
+//         address.state = req.body.state;
+//         address.phone = req.body.phone;
+//         address.save(function(err) {
+//           if (err) { return next(err); } 
+//           res.redirect('/user/address');
+//         });  
+//       });
+//     }
+//   });
+// });
 
 // Set address as default.
 router.put('/address/default/:addressId', checkPermission, (req, res, next)=>{
