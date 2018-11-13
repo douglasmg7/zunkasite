497f52b38f5f5c8778b525ec545e962c4a92e3fb
douglasmg7 <douglasmg7@gmail.com>
2018-11-13 09:30:01 -0200

    added favicon and logo brand

--- app.js
+++ app.js
@@ -125,6 +125,7 @@ app.use('/node_modules', express.static(path.join(__dirname, 'node_modules/')));
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'dist', 'favicon.jpeg')));
 
 // // webpack HMR
 // app.use(webpackDevMiddleware);
