2f98bc7a25e885fc4d18b94d3d200f1e8010af2f
douglasmg7 <douglasmg7@gmail.com>
2019-03-07 11:27:12 -0300

    creating menu using bulma

--- routes/routeSite.js
+++ routes/routeSite.js
@@ -16,6 +16,24 @@ function formatMoney(val){
 }
 
 // Get products page by class (news, more selled...).
+router.get('/bulma', function(req, res, next) {
+  redis.get('banners', (err, banners)=>{
+    // Internal error.
+    if (err) { 
+      log.error(err.stack);
+      return res.render('/error', { message: 'Não foi possível encontrar os banners.', error: err });
+    } 
+    // Render page.  
+    return res.render('product/productListBulma', {
+      nav: {
+      },
+      search: req.query.search ? req.query.search : '',
+      banners: JSON.parse(banners) || [],
+    }); 
+  }); 
+});
+
+// Get products page by class (news, more selled...).
 router.get('/', function(req, res, next) {
   redis.get('banners', (err, banners)=>{
     // Internal error.
