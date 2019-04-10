fbc7c883fb0492b608ed8e17f5d9489de3447f7b
douglasmg7 <douglasmg7@gmail.com>
2019-04-10 10:12:14 -0300

    some user views using the new layout

---
+++ dist/js/user/accountDeleted.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Search for products.
+function _search(text){
+  window.location.href = `/all?page=1&search=${text}`;
+}
+
+// Vue.
+var app = new Vue({
+  el: '#app',
+  data: {
+  },
+});
