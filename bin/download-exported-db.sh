0ac511c35638ad6d8123642fc9082d25b4e67ab5
douglasmg7 <douglasmg7@gmail.com>
2019-06-17 12:16:40 -0300

    download exported db script

---
+++ bin/download-exported-db.sh
@@ -0,0 +1,2 @@
+# scp douglasmg7@vps10092.publiccloud.com.br:/home/douglasmg7/zunka/dump/2018-Nov-27@18:49:28.gz .
+scp douglasmg7@vps10092.publiccloud.com.br:/home/douglasmg7/zunka/dump/2019-Jun-17@*.gz .
