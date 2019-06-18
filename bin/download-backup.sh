facf0b6a84c8772cc2eae0de9652367dc7797cae
douglasmg7 <douglasmg7@gmail.com>
2019-06-18 10:18:41 -0300

    download backup from package.json

--- bin/download-backup.sh
+++ bin/download-backup.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-printf "Get only today exported data\n"
+# Go to script dir.
+cd $(dirname $0)
+
+printf "Get only data exported today.\n"
 # scp douglasmg7@vps10092.publiccloud.com.br:/home/douglasmg7/zunka/dump/2018-Nov-27@18:49:28.gz .
 
 # Get image backup.
 # scp douglasmg7@vps10092.publiccloud.com.br:/home/douglasmg7/zunka/dump/img-2019-Jun-18@*.gz .
-scp douglasmg7@vps10092.publiccloud.com.br:/home/douglasmg7/zunka/dump/img-$(date +%Y-%h-%d)@*.gz .
+scp douglasmg7@vps10092.publiccloud.com.br:/home/douglasmg7/zunka/dump/img-$(date +%Y-%h-%d)@*.gz ../dump/
 
 # Get db backup.
 # scp douglasmg7@vps10092.publiccloud.com.br:/home/douglasmg7/zunka/dump/db-2019-Jun-18@*.gz .
-scp douglasmg7@vps10092.publiccloud.com.br:/home/douglasmg7/zunka/dump/db-$(date +%Y-%h-%d)@*.gz .
+scp douglasmg7@vps10092.publiccloud.com.br:/home/douglasmg7/zunka/dump/db-$(date +%Y-%h-%d)@*.gz ../dump/
