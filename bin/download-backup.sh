91407a1fb97f8a6cdf3a5228444395247bc3f873
douglasmg7 <douglasmg7@gmail.com>
2019-06-18 10:08:14 -0300

    using today date to get backup

--- bin/download-backup.sh
+++ bin/download-backup.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+
+printf "Get only today exported data\n"
 # scp douglasmg7@vps10092.publiccloud.com.br:/home/douglasmg7/zunka/dump/2018-Nov-27@18:49:28.gz .
-scp douglasmg7@vps10092.publiccloud.com.br:/home/douglasmg7/zunka/dump/img-2019-Jun-18@*.gz .
-scp douglasmg7@vps10092.publiccloud.com.br:/home/douglasmg7/zunka/dump/db-2019-Jun-18@*.gz .
+
+# Get image backup.
+# scp douglasmg7@vps10092.publiccloud.com.br:/home/douglasmg7/zunka/dump/img-2019-Jun-18@*.gz .
+scp douglasmg7@vps10092.publiccloud.com.br:/home/douglasmg7/zunka/dump/img-$(date +%Y-%h-%d)@*.gz .
+
+# Get db backup.
+# scp douglasmg7@vps10092.publiccloud.com.br:/home/douglasmg7/zunka/dump/db-2019-Jun-18@*.gz .
+scp douglasmg7@vps10092.publiccloud.com.br:/home/douglasmg7/zunka/dump/db-$(date +%Y-%h-%d)@*.gz .
