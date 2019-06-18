7e9ac7ad833a077a06d9cd73b3856ed1f60f36ca
douglasmg7 <douglasmg7@gmail.com>
2019-06-18 12:40:49 -0300

    redis backup

--- bin/create-backup.sh
+++ bin/create-backup.sh
@@ -7,6 +7,10 @@ cd $(dirname "$0")
 # tar cvzf ../dump/img-$(date +%Y-%h-%d@%T).tar.gz ../dist/img/ ../dist/banner/   # Tar err if this name format.
 tar cvzf ../dump/img-$(date +%Y-%h-%d).tar.gz ../dist/img/ ../dist/banner/
 
-# Backup db.
+# Mongo backup.
 # mongodump --db zunka -u admin --authenticationDatabase admin --gzip --archive=../dump/db-$(date +%Y-%h-%d@%T).gz    # Tar err if this name format.
 mongodump --db zunka -u admin --authenticationDatabase admin --gzip --archive=../dump/db-$(date +%Y-%h-%d).gz
+
+# Redis backup.
+redis-cli save
+sudo cp var/lib/redis/dump.rdb ../dump/dump-$(date +%Y-%h-%d).rdb
