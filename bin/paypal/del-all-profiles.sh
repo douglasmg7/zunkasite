4bc28633001b62ac66f319b2355df56912851491
douglasmg7 <douglasmg7@gmail.com>
2019-08-06 08:30:17 -0300

    remove all web profiles from paypal web server

---
+++ bin/paypal/del-all-profiles.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+read -r URL TYPE TOKEN <<< $(./token.sh $1)
+
+for ID in `./list-profiles.sh | jq '.[] | .id' | sed 's/"//g'`
+do
+	RES=$(curl -s -X DELETE ${URL}payment-experience/web-profiles/${ID} \
+	-H "Content-Type: application/json" \
+	-H "Authorization: $TYPE $TOKEN")
+
+	echo $RES
+done
