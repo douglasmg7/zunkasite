f058d8d70c6a44f6084f19481e9b51e8fbbe0ffa
douglasmg7 <douglasmg7@gmail.com>
2019-08-09 10:51:15 -0300

    script to start services at start-up

---
+++ start-dev.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Go to script dir.
+cd $(dirname "$0") 
+
+[[ `systemctl status mongodb | awk '/Active/{print $2}'` == inactive ]] && sudo systemctl start mongodb
+[[ `systemctl status redis | awk '/Active/{print $2}'` == inactive ]] && sudo systemctl start redis
+npm run dev
