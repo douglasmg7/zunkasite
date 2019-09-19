6e4087828a219335ca02a18e9ffa601f653b32b7
douglasmg7 <douglasmg7@gmail.com>
2019-09-19 14:27:31 -0300

    new contact number

--- routes/routeSetup.js
+++ routes/routeSetup.js
@@ -2,11 +2,32 @@
 const express = require('express');
 const router = express.Router();
 const log = require('../config/log');
+const { check, validationResult } = require('express-validator/check');
 
-// Get Product.
-router.post('/product/add', (req, res, next)=>{
+const Product = require('../model/product');
+
+
+
+// Add product.
+router.post('/product/add', [
+		check('dealerName').isLength({ min: 5 })
+], (req, res, next)=>{
 	log.debug(JSON.stringify(req.body, null, 3));
+
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		log.debug('erro!');
+		return res.status(422).json({ errors: errors.array() });
+	}
+
+	let product = new Product();
+	// Dealer name.
+	product.dealerName = req.body.dealerName;
+
+
+	log.debug(`product: ${JSON.stringify(product)}`);
 	res.json(' success: true ');
+
 		
 	// res.render('producstoretsStore', { user: req.isAuthenticated() ? req.user : { username: undefined, group: undefined }, csrfToken: req.csrfToken() });
 });
