01e91a019fca8a49f4cf753fcda202be74703e66
douglasmg7 <douglasmg7@gmail.com>
2019-09-21 16:50:00 -0300

    ppp frame not hidding continue button

--- routes/routeSetup.js
+++ routes/routeSetup.js
@@ -10,26 +10,50 @@ const Product = require('../model/product');
 
 // Add product.
 router.post('/product/add', [
-		check('dealerName').isLength({ min: 5 })
+		check('dealerName').isLength(4, 20),
+		check('dealerProductId').isLength(1, 20),
+		check('dealerProductTitle').isLength(4, 200),
+		check('dealerProductDesc').isLength(4, 2000),
+		check('dealerProductBrand').isLength(4, 200),
+		check('dealerProductWarrantyDays').isNumeric(),
+		check('dealerProductDeep').isNumeric(),
+		check('dealerProductHeight').isNumeric(),
+		check('dealerProductWidth').isNumeric(),
+		check('dealerProductWeight').isNumeric(),
+		check('dealerProductActive').isBoolean(),
+		check('dealerProductPrice').isNumeric(),
+		check('dealerProductLastUpdate').isISO8601(),
 ], (req, res, next)=>{
-	log.debug(JSON.stringify(req.body, null, 3));
-
+	// log.debug(JSON.stringify(req.body, null, 3));
+	// Check erros.
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
-		log.debug('erro!');
-		return res.status(422).json({ errors: errors.array() });
+		// log.debug(JSON.stringify(errors.array(), null, 2));
+		return res.status(422).json({ erros: errors.array() });
 	}
-
+	// Create product.
 	let product = new Product();
-	// Dealer name.
 	product.dealerName = req.body.dealerName;
-
-
-	log.debug(`product: ${JSON.stringify(product)}`);
+	product.dealerProductId = req.body.dealerProductId;
+	product.dealerProductTitle = req.body.dealerProductTitle;
+	product.dealerProductDesc = req.body.dealerProductDesc;
+	product.dealerProductBrand = req.body.dealerProductBrand;
+	product.dealerProductWarrantDays = req.body.dealerProductWarrantDays;
+	product.dealerProductDeep = req.body.dealerProductDeep;
+	product.dealerProductHeight = req.body.dealerProductHeight;
+	product.dealerProductWidth = req.body.dealerProductWidth;
+	product.dealerProductWeight = req.body.dealerProductWeight;
+	product.dealerProductActive = req.body.dealerProductActive;
+	product.dealerProductPrice = req.body.dealerProductPrice;
+	product.dealerProductLastUpdate = req.body.dealerProductLastUpdate;
+	log.debug(`product: ${JSON.stringify(product, null, 2)}`);
+	// Save product.
+	product.save(err=>{
+		if (err) {
+			return res.status(500).send(err);
+		}
+	});
 	res.json(' success: true ');
-
-		
-	// res.render('producstoretsStore', { user: req.isAuthenticated() ? req.user : { username: undefined, group: undefined }, csrfToken: req.csrfToken() });
 });
 
 // // Store products.
