d710dde942e5777893ca87fcc4fc2af76c044cb7
douglasmg7 <douglasmg7@gmail.com>
2019-10-15 13:16:15 -0300

    calc markup selecting correct orders

--- bin/calcTotalSold.js
+++ bin/calcTotalSold.js
@@ -4,14 +4,19 @@ const mongoose = require('../db/mongoose');
 const Order = require('../model/order');
 const log = require('../config/log');
 
+function toBrMoney(val) {
+    return 'R$' + val.toFixed(0).replace('.', ',').replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+};
+
 // Remove old values.
-log.info('Geting shipped, delivered and paid orders...');
+log.info('Getting paid, shipped and delivered orders...');
 
 // Total.
 let totalSold = 0;
 let statusMap = new Map();
 
-Order.find({status: {$nin: ["placed", "canceled"]}}).sort({ "timestamps.paidAt": 1 })
+// Order.find({status: {$nin: ["placed", "canceled"]}}).sort({ "timestamps.paidAt": 1 })
+Order.find({status: {$in: ["paid", "shipped", "delivered"]}}).sort({ "timestamps.paidAt": 1 })
 .then(docs=>{
     docs.forEach(order=>{
         // log.debug(`${parseFloat(order.subtotalPrice)}`);
@@ -19,12 +24,15 @@ Order.find({status: {$nin: ["placed", "canceled"]}}).sort({ "timestamps.paidAt":
         statusMap.set(order.status, true);
         let tmpDate = new Date(order.timestamps.placedAt);
         log.debug(`${totalSold.toFixed(0)} (${parseFloat(order.subtotalPrice).toFixed(0)})  ->  ${tmpDate.toDateString()} - ${order.name} `);
+        // log.debug(`${totalSold.toFixed(0)} (${parseFloat(order.subtotalPrice).toFixed(0)})  ->  ${order.timestamps.placedAt} - ${order.name} `);
+        // log.debug(`${totalSold.toFixed(0)} (${parseFloat(order.subtotalPrice).toFixed(0)})  ->  ${order.timestamps} - ${order.name} `);
     });
     statusMap.forEach((val, key)=>{
         log.debug(`- ${key} -`);
     });
-    log.debug(`sold: ${totalSold.toFixed(0)}`);
-    log.debug(`markup: ${(totalSold * .3).toFixed(0)}`);
+    log.debug(`Quantity: ${docs.length}`);
+    log.debug(`sold: ${toBrMoney(totalSold)}`);
+    log.debug(`markup: ${toBrMoney(totalSold * .3)}`);
     mongoose.close(()=>{
         process.exit();
     });
@@ -32,3 +40,4 @@ Order.find({status: {$nin: ["placed", "canceled"]}}).sort({ "timestamps.paidAt":
 .catch(err=>{
     log.error(err);
 });
+
