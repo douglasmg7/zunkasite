043f3227b1d92e4d16256e04f208f6d78e35a4ec
douglasmg7 <douglasmg7@gmail.com>
2019-10-15 11:42:29 -0300

    calc markup date and name

--- bin/calcTotalSold.js
+++ bin/calcTotalSold.js
@@ -11,12 +11,14 @@ log.info('Geting shipped, delivered and paid orders...');
 let totalSold = 0;
 let statusMap = new Map();
 
-Order.find({status: {$nin: ["placed", "canceled"]}})
+Order.find({status: {$nin: ["placed", "canceled"]}}).sort({ "timestamps.paidAt": 1 })
 .then(docs=>{
     docs.forEach(order=>{
         // log.debug(`${parseFloat(order.subtotalPrice)}`);
         totalSold = totalSold + parseFloat(order.subtotalPrice);
         statusMap.set(order.status, true);
+        let tmpDate = new Date(order.timestamps.placedAt);
+        log.debug(`${totalSold.toFixed(0)} (${parseFloat(order.subtotalPrice).toFixed(0)})  ->  ${tmpDate.toDateString()} - ${order.name} `);
     });
     statusMap.forEach((val, key)=>{
         log.debug(`- ${key} -`);
