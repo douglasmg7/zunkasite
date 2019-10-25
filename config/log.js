38215cb2e0f1b17a435b6250ad50fd5d19abc33d
douglasmg7 <douglasmg7@gmail.com>
2019-10-25 15:28:59 -0300

    pm2 launch zunkasrv, zunkasite using ZUNKAPATH  for logs

--- config/log.js
+++ config/log.js
@@ -6,9 +6,17 @@ const { createLogger, format, transports, addColors } = require('winston');
 const fs = require('fs');
 const colors = require('colors/safe');
 // file to write logs.
-const parsePath = path.parse(module.parent.filename);
-const logDir = path.join(parsePath.dir, 'log');
-const logFilename = path.join(logDir, (parsePath.name + '.log'));
+// const parsePath = path.parse(module.parent.filename);
+// const logDir = path.join(parsePath.dir, 'log');
+// const logFilename = path.join(logDir, (parsePath.name + '.log'));
+
+// ZUNKAPATH must be defined.
+if (!process.env.ZUNKAPATH) {
+    console.error(`ZUNKAPATH not defined.`);
+    process.exit(1);
+} 
+const logDir = path.join(process.env.ZUNKAPATH, 'log');
+const logFilename = path.join(logDir, 'zunkasite.log');
 // console.log(logDir);
 // console.log(logFilename);
 
@@ -55,7 +63,7 @@ const log = createLogger({
   ]
 });
 
-// No test mode.
+// Not run on test mode.
 if(process.env.NODE_ENV !== 'test'){
   log.add(new transports.Console({
     level: 'debug',
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,29 +1,30 @@
 module.exports = {
-  apps : [{
-    name: 'zunka_server',
-    script: './bin/www',
-    watch: false,
-    env: {
-      NODE_ENV: 'development'
+    apps : [{
+        name: 'zunka_site',
+        script: './bin/www',
+        watch: false,
+        env: {
+            NODE_ENV: 'development'
+        },
+        env_production: {
+            NODE_ENV: 'production',
+            DB: 'production'
+        },
+        output: '/dev/null',
+        error: process.env.ZUNKAPATH + '/log/pm2_zunkasite.log',
+        log: '/dev/null',
+        // log_type: "json",
+        // log: './log/pm2_combined.out_err.log',
     },
-    env_production: {
-      NODE_ENV: 'production',
-      DB: 'production'
-    },
-    output: '/dev/null',
-    error: './log/pm2.log',
-    log: '/dev/null',
-    // log_type: "json",
-    // log: './log/pm2_combined.out_err.log',
-  }],
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    }
-  }
+    {
+        name: 'zunka_srv',
+        script: 'zunkasrv',
+        cwd: process.env.GS + '/zunkasrv',
+        watch: false,
+        env: {},
+        env_production: {},
+        output: '/dev/null',
+        error: process.env.ZUNKAPATH + '/log/pm2_zunkasrv.log',
+        log: '/dev/null',
+    }]
 };
