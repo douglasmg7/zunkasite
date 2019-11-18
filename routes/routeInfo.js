e0eff1aaf00e5fa40adaff2f97eda3c202c88a46
douglasmg7 <douglasmg7@gmail.com>
2019-11-18 10:04:54 -0300

    Using markdown for all infos.
    Markdown edit page:
        Autofocus on field name.
        Markdwon textarea bigger.
    
    Page select address:
        Voçê corrected to você.

--- routes/routeInfo.js
+++ routes/routeInfo.js
@@ -7,8 +7,15 @@ const markdownCache = require('../model/markdownCache');
 
 // About company.
 router.get('/about-company', (req, res, next)=>{
+    // Warranty text.
+    let text = '';
+    let markdownText = markdownCache.getCache().get('info-sobre-a-empresa');
+    if (markdownText) {
+        text = marked(markdownText);
+    }
     res.render('info/aboutCompany', {
         nav: {},
+        text
     });
 })
 
@@ -16,7 +23,7 @@ router.get('/about-company', (req, res, next)=>{
 router.get('/contact', (req, res, next)=>{
     // Warranty text.
     let text = '';
-    let markdownText = markdownCache.getCache().get('contato');
+    let markdownText = markdownCache.getCache().get('info-contato');
     if (markdownText) {
         text = marked(markdownText);
     }
@@ -28,36 +35,66 @@ router.get('/contact', (req, res, next)=>{
 
 // Payment.
 router.get('/payment', (req, res, next)=>{
+    let text = '';
+    let markdownText = markdownCache.getCache().get('info-pagamento');
+    if (markdownText) {
+        text = marked(markdownText);
+    }
     res.render('info/payment', {
         nav: {},
+        text
     });
 })
 
 // How to buy.
 router.get('/how-to-buy', (req, res, next)=>{
+    let text = '';
+    let markdownText = markdownCache.getCache().get('info-como-comprar');
+    if (markdownText) {
+        text = marked(markdownText);
+    }
     res.render('info/howToBuy', {
         nav: {},
+        text
     });
 })
 
 // Retrun policy.
 router.get('/return', (req, res, next)=>{
+    let text = '';
+    let markdownText = markdownCache.getCache().get('info-devolução');
+    if (markdownText) {
+        text = marked(markdownText);
+    }
     res.render('info/return', {
         nav: {},
+        text
     });
 })
 
 // Warranty policy.
 router.get('/warranty', (req, res, next)=>{
+    let text = '';
+    let markdownText = markdownCache.getCache().get('info-garantia');
+    if (markdownText) {
+        text = marked(markdownText);
+    }
     res.render('info/warranty', {
         nav: {},
+        text
     });
 })
 
 // About site.
 router.get('/about-site', (req, res, next)=>{
+    let text = '';
+    let markdownText = markdownCache.getCache().get('info-sobre-o-site');
+    if (markdownText) {
+        text = marked(markdownText);
+    }
     res.render('info/aboutSite', {
         nav: {},
+        text
     });
 })
 
