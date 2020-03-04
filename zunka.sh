71b0e3e1cb054f8b40391b7973eedb47069a3913
douglasmg7 <douglasmg7@gmail.com>
2020-03-04 13:06:53 -0300

    GS env modified

--- zunka.sh
+++ zunka.sh
@@ -7,12 +7,12 @@ export PATH=$PATH:$GOPATH/bin
 
 # Golang source is out gopath and using mod.
 # export GS=$GOPATH/src/github.com/douglasmg7
-export /home/douglasmg7/code
+export GS=/home/douglasmg7/code
 
 # Zunka.
 export ZUNKAPATH=~/.local/share/zunka
 # export ZUNKA_SITE_PATH=/home/douglasmg7/zunka
-export ZUNKA_SITE_PATH=/home/douglasmg7/code/zunkasite
+export ZUNKA_SITE_PATH=$GS/zunkasite
 export ZUNKA_SRV_DB=zunkasrv.db
 export ZUNKA_ALDOWSC_DB=aldowsc.db
 export ZUNKA_FREIGHT_DB=freight.db
