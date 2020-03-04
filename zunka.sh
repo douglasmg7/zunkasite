396af05389d23a4f7dcacac332c02de36d62d2f5
douglasmg7 <douglasmg7@gmail.com>
2020-03-04 11:42:12 -0300

    Chande porject folder

--- zunka.sh
+++ zunka.sh
@@ -5,12 +5,14 @@ export PATH=$PATH:/usr/local/go/bin
 export GOPATH=$HOME/code/golang
 export PATH=$PATH:$GOPATH/bin
 
-# Dmg golang src.
-export GS=$GOPATH/src/github.com/douglasmg7
+# Golang source is out gopath and using mod.
+# export GS=$GOPATH/src/github.com/douglasmg7
+export /home/douglasmg7/code
 
 # Zunka.
 export ZUNKAPATH=~/.local/share/zunka
-export ZUNKA_SITE_PATH=/home/douglasmg7/zunka
+# export ZUNKA_SITE_PATH=/home/douglasmg7/zunka
+export ZUNKA_SITE_PATH=/home/douglasmg7/code/zunkasite
 export ZUNKA_SRV_DB=zunkasrv.db
 export ZUNKA_ALDOWSC_DB=aldowsc.db
 export ZUNKA_FREIGHT_DB=freight.db
