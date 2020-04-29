af5c34f423ee8a460d552490323475df27adbf45
douglasmg7 <douglasmg7@gmail.com>
2020-04-29 15:09:12 -0300

    update scripts

--- bin/system/update-system.sh
+++ bin/system/update-system.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
+[[ -z $ZUNKA_SITE_PATH ]] && printf "error: ZUNKA_SITE_PATH enviorment not defined.\n" >&2 && exit 1 
 
-cd $(dirname "$0")
 printf "\n\n\n\nStarting update - %s \n" "$(date)"
-./_update-zunkasite.sh
-./_update-golang-services.sh
-. ./_restart-updated-systems.sh
+ls -la  $ZUNKA_SITE_PATH/bin/system/_update-zunkasite.sh
+
+exit
+. $ZUNKA_SITE_PATH/bin/system/_update-zunkasite.sh
+. $ZUNKA_SITE_PATH/bin/system/_update-golang-services.sh
+. $ZUNKA_SITE_PATH/bin/system/_restart-updated-systems.sh
+
+# ./_update-zunkasite.sh
+# ./_update-golang-services.sh
+# . ./_restart-updated-systems.sh
