a2f15311f11f1db5e10dbd19338593e483909d8b
douglasmg7 <douglasmg7@gmail.com>
2020-05-08 13:57:10 -0300

    point to zoom dev

--- util/zoom.js
+++ util/zoom.js
@@ -20,6 +20,8 @@ function getZoomOrder(orderId, cb){
         return cb(null, zoomOrderJson);
     } else {
         // log.debug('Getting zoom order from zoom server.');
+        // todo - comment and redirect s.zoom to production.
+        log.debug(`Getting zoom order from zoom server. URL: ${s.zoom.host}/order/${orderId}`);
         axios.get(`${s.zoom.host}/order/${orderId}`, {
             headers: {
                 "Accept": "application/json", 
