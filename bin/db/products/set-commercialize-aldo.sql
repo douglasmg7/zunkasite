8755fb8a18bed0cb49b43314c90ff81ccb6acd7b
douglasmg7 <douglasmg7@gmail.com>
2020-06-07 11:16:41 -0300

    Script to set aldo products to commercialize.

---
+++ bin/db/products/set-commercialize-aldo.sql
@@ -0,0 +1,29 @@
+print("");
+print("Changing batch size to:");
+DBQuery.shellBatchSize = 10000
+
+print("");
+print("Aldo products set to commercialize")
+db.products.find({dealerName: "Aldo", storeProductCommercialize: true }).count();
+db.products.find({dealerName: "Aldo", storeProductCommercialize: true },{ _id: true, dealerName: true, dealerProductActive: true, updatedAt: true});
+
+print("");
+print("Aldo products not set to commercialize")
+db.products.find({dealerName: "Aldo", storeProductCommercialize: false }).count();
+db.products.find({dealerName: "Aldo", storeProductCommercialize: false },{ _id: true, dealerName: true, dealerProductActive: true, updatedAt: true});
+
+print("");
+print("Setting Aldo products to commercialize")
+db.products.updateMany({dealerName: "Aldo", storeProductCommercialize: false, dealerProductActive: true}, { "$set": {"storeProductCommercialize": true}, "$currentDate": {"updatedAt": true} })
+
+print("");
+print("After command");
+print("");
+print("Aldo products set to commercialize")
+db.products.find({dealerName: "Aldo", storeProductCommercialize: true }).count();
+db.products.find({dealerName: "Aldo", storeProductCommercialize: true },{ _id: true, dealerName: true, dealerProductActive: true, updatedAt: true});
+
+print("");
+print("Aldo products not set to commercialize")
+db.products.find({dealerName: "Aldo", storeProductCommercialize: false }).count();
+db.products.find({dealerName: "Aldo", storeProductCommercialize: false },{ _id: true, dealerName: true, dealerProductActive: true, updatedAt: true});
