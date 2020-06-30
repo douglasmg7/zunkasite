d889646a736b9f9c90f7161947dcf33230e90df9
douglasmg7 <douglasmg7@gmail.com>
2020-06-30 09:51:14 -0300

    added sqlite dbs to backup list

--- bin/backup/create-backup.sh
+++ bin/backup/create-backup.sh
@@ -1,18 +1,97 @@
 #!/usr/bin/env bash
 
-# Go to script dir.
-cd $(dirname "$0") 
+# ZUNKAPATH not defined.
+if [ -z "$ZUNKAPATH" ]; then
+	printf "error: ZUNKAPATH not defined.\n" >&2
+	exit 1 
+fi
 
-# Backup images.
-# tar cvzf ../dump/img-$(date +%Y-%h-%d@%T).tar.gz ../dist/img/ ../dist/banner/   # Tar err if this name format.
-tar cvzf ../dump/img-$(date +%Y-%h-%d).tar.gz ../dist/img/ ../dist/banner/
+# ZUNKA_FREIGHT_DB not defined.
+if [ -z "$ZUNKA_FREIGHT_DB" ]; then
+	printf "error: ZUNKA_FREIGHT_DB not defined.\n" >&2
+	exit 1 
+fi
+
+# ZUNKA_SRV_DB not defined.
+if [ -z "$ZUNKA_SRV_DB" ]; then
+	printf "error: ZUNKA_SRV_DB not defined.\n" >&2
+	exit 1 
+fi
+
+# ZUNKA_FREIGHT_DB not defined.
+if [ -z "$ZUNKA_FREIGHT_DB" ]; then
+	printf "error: ZUNKA_FREIGHT_DB not defined.\n" >&2
+	exit 1 
+fi
+
+# ZUNKA_SITE_PATH not defined.
+if [ -z "$ZUNKA_SITE_PATH" ]; then
+	printf "error: ZUNKA_SITE_PATH not defined.\n" >&2
+	exit 1 
+fi
+
+FREIGHT_FILE_DB=$ZUNKAPATH/db/$ZUNKA_FREIGHT_DB
+ZUNKASRV_FILE_DB=$ZUNKAPATH/db/$ZUNKA_SRV_DB
+ALDOWSC_FILE_DB=$ZUNKAPATH/db/$ZUNKA_ALDOWSC_DB
+
+# Backup dir.
+BACKUP_DIR=$ZUNKA_SITE_PATH/dump/$(date +%Y-%h-%d)
+
+# echo $FREIGHT_FILE_DB
+# echo $ZUNKASRV_FILE_DB
+# echo $ALDOWSC_FILE_DB
+# echo $BACKUP_DIR
+# echo $FREIGHT_BACKUP
+# echo $ZUNKASRV_BACKUP
+# echo $ALDOWSC_BACKUP
+
+# Check if freight db exist.
+if [[ ! -f $FREIGHT_FILE_DB ]]; then
+    printf "error: $FREIGHT_FILE_DB not exist.\n" >&2
+    exit
+fi
+
+# Check if zunkasrv db exist.
+if [[ ! -f $ZUNKASRV_FILE_DB ]]; then
+    printf "error: $ZUNKASRV_FILE_DB not exist.\n" >&2
+    exit
+fi
+
+# Check if aldowsc db exist.
+if [[ ! -f $ALDOWSC_FILE_DB ]]; then
+    printf "error: $ALDOWSC_FILE_DB not exist.\n" >&2
+    exit
+fi
+
+# Create backup dir.
+mkdir -p $BACKUP_DIR
+
+echo Creating $FREIGHT_FILE_DB backup...
+sqlite3 $FREIGHT_FILE_DB ".backup $BACKUP_DIR/sqlite3.freight.backup"
+
+echo Creating $ZUNKASRV_FILE_DB backup...
+sqlite3 $ZUNKASRV_FILE_DB ".backup $BACKUP_DIR/sqlite3.zunkasrv.backup"
+
+echo Creating $ALDOWSC_FILE_DB backup...
+sqlite3 $ALDOWSC_FILE_DB ".backup $BACKUP_DIR/sqlite3.aldowsc.backup"
 
 # Mongo backup.
 # mongodump --db zunka -u admin --authenticationDatabase admin --gzip --archive=../dump/db-$(date +%Y-%h-%d@%T).gz    # Tar err if this name format.
+echo Creating mongodb backup...
 printf "\nMongo db admin password:\n"
-mongodump --db zunka -u admin --authenticationDatabase admin --gzip --archive=../dump/db-$(date +%Y-%h-%d).gz
+mongodump --db zunka -u admin --authenticationDatabase admin --gzip --archive=$BACKUP_DIR/mongodb.gz
 
 # Redis backup.
+echo Creating redis backup...
 redis-cli save
 printf "\nroot password:\n"
-sudo cp /var/lib/redis/dump.rdb ../dump/dump-$(date +%Y-%h-%d).rdb
+sudo cp /var/lib/redis/dump.rdb ../dump/$BACKUP_DIR/redis.rdb
+
+# Backup images.
+# tar cvzf ../dump/img-$(date +%Y-%h-%d@%T).tar.gz ../dist/img/ ../dist/banner/   # Tar err if this name format.
+# tar cvzf ../dump/img-$(date +%Y-%h-%d).tar.gz ../dist/img/ ../dist/banner/
+# tar cvzf ../dump/$(date +%Y-%h-%d)-img.tar.gz ../dist/img/ ../dist/banner/
+echo Creating images backup...
+tar cvzf $BACKUP_DIR/img.tar.gz $ZUNKA_SITE_PATH/dist/img/ $ZUNKA_SITE_PATH/dist/banner/
+
+echo Finished
--- bin/backup/download-backup.sh
+++ bin/backup/download-backup.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
-# Go to script dir.
-cd $(dirname $0)
+# ZUNKA_SITE_PATH not defined.
+if [ -z "$ZUNKA_SITE_PATH" ]; then
+	printf "error: ZUNKA_SITE_PATH not defined.\n" >&2
+	exit 1 
+fi
 
-printf "Get only data exported today.\n"
-# scp douglasmg7@vps10092.publiccloud.com.br:/home/douglasmg7/zunka/dump/2018-Nov-27@18:49:28.gz .
+while true; do
+    read -p "Which date to use to download backup? [ENTER] for $(date +%Y-%m-%d) or yyyy-mm-dd to custom date: " ANSWER
+    if [[ $ANSWER =~ [0-9]{4}-[0-9]{2}-[0-9]{2} ]]; then
+        BACKUP_DIR=$ZUNKA_SITE_PATH/dump/$ANSWER
+        break
+    elif [[ -z $ANSWER ]]; then
+        BACKUP_DIR=$ZUNKA_SITE_PATH/dump/$(date +%Y-%m-%d)
+        break
+    else
+        echo "Invalid date: $ANSWER"
+    fi
+done
 
-# Get images backup.
-scp douglasmg7@vps10092.publiccloud.com.br:/home/douglasmg7/code/zunkasite/dump/img-$(date +%Y-%h-%d).tar.gz ../dump/
-
-# Get mongo backup.
-scp douglasmg7@vps10092.publiccloud.com.br:/home/douglasmg7/code/zunkasite/dump/db-$(date +%Y-%h-%d).gz ../dump/
-
-# Get redis backup.
-scp root@vps10092.publiccloud.com.br:/home/douglasmg7/code/zunkasite/dump/dump-$(date +%Y-%h-%d).rdb ../dump/
+scp -r douglasmg7@vps10092.publiccloud.com.br:$BACKUP_DIR $BACKUP_DIR
