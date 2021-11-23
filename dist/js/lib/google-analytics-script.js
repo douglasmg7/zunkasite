8c6bd4e82fe94bcbd3ff7cdbeffcb5274911bb87
douglasmg7 <douglasmg7@gmail.com>
2021-11-23 10:15:18 -0300

    analytic script forgotten

---
+++ dist/js/lib/google-analytics-script.js
@@ -0,0 +1,6 @@
+console.log('oi 2');
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-213460703-1');
+console.log('oi end');
