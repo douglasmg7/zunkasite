1534e2f266f08a7ddf006bbfd65e9d4f2db8a047
douglasmg7 <douglasmg7@gmail.com>
2022-02-17 10:08:01 -0300

    import backup for docker mongo

---
+++ bin/backup/import-backup-development.sh
@@ -0,0 +1,225 @@
+#!/usr/bin/env bash
+
+# ZUNKAPATH not defined.
+if [ -z "$ZUNKAPATH" ]; then
+	printf "error: ZUNKAPATH not defined.\n" >&2
+	exit 1 
+fi
+
+# ZUNKA_FREIGHT_DB not defined.
+if [ -z "$ZUNKA_FREIGHT_DB" ]; then
+	printf "error: ZUNKA_FREIGHT_DB not defined.\n" >&2
+	exit 1 
+fi
+
+# ZUNKA_SRV_DB not defined.
+if [ -z "$ZUNKA_SRV_DB" ]; then
+	printf "error: ZUNKA_SRV_DB not defined.\n" >&2
+	exit 1 
+fi
+
+# ALLNATIONS_DB not defined.
+if [ -z "$ALLNATIONS_DB" ]; then
+	printf "error: ALLNATIONS_DB not defined.\n" >&2
+	exit 1 
+fi
+
+# ZUNKA_SITE_PATH not defined.
+if [ -z "$ZUNKA_SITE_PATH" ]; then
+	printf "error: ZUNKA_SITE_PATH not defined.\n" >&2
+	exit 1 
+fi
+
+while true; do
+    read -p "Import backup from which date? [ENTER] for $(date +%Y-%m-%d) or yyyy-mm-dd to custom date: " ANSWER
+    if [[ $ANSWER =~ [0-9]{4}-[0-9]{2}-[0-9]{2} ]]; then
+        BACKUP_DIR=$ZUNKA_SITE_PATH/dump/$ANSWER
+        break
+    elif [[ -z $ANSWER ]]; then
+        BACKUP_DIR=$ZUNKA_SITE_PATH/dump/$(date +%Y-%m-%d)
+        break
+    else
+        echo "Invalid date: $ANSWER"
+    fi
+done
+
+# Freight db.
+FREIGHT_FILE_DB=$ZUNKAPATH/db/$ZUNKA_FREIGHT_DB
+FREIGHT_FILE_DB_OLD=$ZUNKAPATH/db/freight-$(date +%Y_%m_%d).db
+FREIGHT_DB_BACKUP=$BACKUP_DIR/sqlite3.freight.backup
+
+# Zunkasrv db.
+ZUNKASRV_FILE_DB=$ZUNKAPATH/db/$ZUNKA_SRV_DB
+ZUNKASRV_FILE_DB_OLD=$ZUNKAPATH/db/zunkasrv-$(date +%Y_%m_%d).db
+ZUNKASRV_DB_BACKUP=$BACKUP_DIR/sqlite3.zunkasrv.backup
+
+# Aldowsc db.
+ALDOWSC_FILE_DB=$ZUNKAPATH/db/$ZUNKA_ALDOWSC_DB
+ALDOWSC_FILE_DB_OLD=$ZUNKAPATH/db/aldowsc-$(date +%Y_%m_%d).db
+ALDOWSC_DB_BACKUP=$BACKUP_DIR/sqlite3.aldowsc.backup
+
+# Allnations db.
+ALLNATIONS_FILE_DB=$ALLNATIONS_DB
+ALLNATIONS_FILE_DB_OLD=$ZUNKAPATH/db/allnations-$(date +%Y_%m_%d).db
+ALLNATIONS_DB_BACKUP=$BACKUP_DIR/sqlite3.allnations.backup
+
+# # Warn freight db exist.
+# if [[ -f $FREIGHT_FILE_DB ]]; then
+    # printf "WARN: $FREIGHT_FILE_DB exist.\n"
+# fi
+
+# # Warn zunkasrv db exist.
+# if [[ -f $ZUNKASRV_FILE_DB ]]; then
+    # printf "WARN: $ZUNKASRV_FILE_DB exist.\n"
+# fi
+
+# # Warn aldowsc db exist.
+# if [[ -f $ALDOWSC_FILE_DB ]]; then
+    # printf "WARN: $ALDOWSC_FILE_DB exist.\n"
+# fi
+
+# # Freight sqlite3 db.
+# while true; do
+    # echo
+    # read -p "Import freight from $FREIGHT_DB_BACKUP to $FREIGHT_FILE_DB? (y/n):" ANSWER
+    # if [[ $ANSWER == y ]]; then
+        # echo "Importing freight db..."
+        # mv $FREIGHT_FILE_DB $FREIGHT_FILE_DB_OLD
+        # sqlite3 $FREIGHT_FILE_DB ".restore $FREIGHT_DB_BACKUP"
+        # break
+    # elif [[ $ANSWER == n ]]; then
+        # echo "Freight db will not be imported"
+        # break
+    # else
+        # echo "Invalid option: $ANSWER"
+    # fi
+# done
+
+# # Zunkasrv sqlite3 db.
+# while true; do
+    # echo
+    # read -p "Import zunkasrv from $ZUNKASRV_DB_BACKUP to $ZUNKASRV_FILE_DB? (y/n):" ANSWER
+    # if [[ $ANSWER == y ]]; then
+        # echo "Importing zunkasrv db..."
+        # mv $ZUNKASRV_FILE_DB $ZUNKASRV_FILE_DB_OLD
+        # sqlite3 $ZUNKASRV_FILE_DB ".restore $ZUNKASRV_DB_BACKUP"
+        # break
+    # elif [[ $ANSWER == n ]]; then
+        # echo "Zunkasrv db will not be imported"
+        # break
+    # else
+        # echo "Invalid option: $ANSWER"
+    # fi
+# done
+
+# # Aldowsc sqlite3 db.
+# while true; do
+    # echo
+    # read -p "Import aldowsc from $ALDOWSC_DB_BACKUP to $ALDOWSC_FILE_DB? (y/n):" ANSWER
+    # if [[ $ANSWER == y ]]; then
+        # echo "Importing aldowsc db..."
+        # mv $ALDOWSC_FILE_DB $ALDOWSC_FILE_DB_OLD
+        # sqlite3 $ALDOWSC_FILE_DB ".restore $ALDOWSC_DB_BACKUP"
+        # break
+    # elif [[ $ANSWER == n ]]; then
+        # echo "Aldowsc db will not be imported"
+        # break
+    # else
+        # echo "Invalid option: $ANSWER"
+    # fi
+# done
+
+# # Allnations sqlite3 db.
+# while true; do
+    # echo
+    # read -p "Import allnations from $ALLNATIONS_DB_BACKUP to $ALLNATIONS_FILE_DB? (y/n):" ANSWER
+    # if [[ $ANSWER == y ]]; then
+        # echo "Importing allnations db..."
+        # mv $ALLNATIONS_FILE_DB $ALLNATIONS_FILE_DB_OLD
+        # sqlite3 $ALLNATIONS_FILE_DB ".restore $ALLNATIONS_DB_BACKUP"
+        # break
+    # elif [[ $ANSWER == n ]]; then
+        # echo "Allanations db will not be imported"
+        # break
+    # else
+        # echo "Invalid option: $ANSWER"
+    # fi
+# done
+
+
+# Import mongodb.
+MONGO_DB_BACKUP=$BACKUP_DIR/mongodb.gz
+while true; do
+    echo
+    read -p "Import mongo db? (y/n):" ANSWER
+    if [[ $ANSWER == y ]]; then
+        # Start docker services.
+        [[ `systemctl status docker | awk '/Active/{print $2}'` == inactive ]] && sudo systemctl start docker
+        sleep .5
+
+        # Create or start mongo container.
+        if [[ -z $(docker ps -a | grep zunka_mongo) ]]
+        # Create and implicit start container.
+        then
+            echo 'Creating and starting zunka_mongo container:'
+            docker run -d --name zunka_mongo -v data:/data/db -p 27017:27017 mongo:3.6.3
+        # Start zunka_mongo container.
+        else
+            echo 'Starting zunka_mongo container:'
+            docker start zunka_mongo
+        fi
+
+        echo "Importing mongo data: $MONGO_DB_BACKUP"
+        # mongorestore -u admin --authenticationDatabase admin --nsInclude zunka.* --nsFrom 'zunka.*' --nsTo 'zunka.*' --gzip --archive=$MONGO_DB_BACKUP
+        docker exec -i zunka_mongo sh -c 'mongorestore --gzip --archive' < $MONGO_DB_BACKUP
+        break
+    elif [[ $ANSWER == n ]]; then
+        echo "Mongo db will not be imported"
+        break
+    else
+        echo "Invalid option: $ANSWER"
+    fi
+done
+
+# # Import redis db.
+# REDIS_DB_BACKUP=$BACKUP_DIR/redis.rdb
+# while true; do
+    # echo
+    # read -p "Import redis db? (y/n):" ANSWER
+    # if [[ $ANSWER == y ]]; then
+        # echo "Importing redis db..."
+        # printf "\nroot password:\n"
+        # sudo systemctl stop redis
+        # sudo mv /var/lib/redis/dump.rdb /var/lib/redis/dump-$(date +%Y-%m-%d).rdb.old
+        # sudo cp -p $REDIS_DB_BACKUP /var/lib/redis/dump.rdb
+        # sudo chown redis:redis /var/lib/redis/dump.rdb
+        # sudo chmod 660 /var/lib/redis/dump.rdb
+        # sudo systemctl start redis
+        # break
+    # elif [[ $ANSWER == n ]]; then
+        # echo "Redis db will not be imported"
+        # break
+    # else
+        # echo "Invalid option: $ANSWER"
+    # fi
+# done
+
+# Import images.
+IMAGES_BACKUP=$BACKUP_DIR/img.tar.gz
+while true; do
+    echo
+    read -p "Import images? (y/n):" ANSWER
+    if [[ $ANSWER == y ]]; then
+        echo Importing images.
+        tar zxvf $BACKUP_DIR/img.tar.gz -C $ZUNKA_SITE_PATH
+        break
+    elif [[ $ANSWER == n ]]; then
+        echo "Images will not be imported"
+        break
+    else
+        echo "Invalid option: $ANSWER"
+    fi
+done
+
+echo
+echo Import finished
--- bin/start-development_new.sh
+++ bin/start-development_new.sh
@@ -11,34 +11,47 @@
 sleep .5
 
 # Create or start mongo container.
-if [[ -z $(docker ps -a | grep mongo_zunka) ]]
+if [[ -z $(docker ps -a | grep zunka_mongo) ]]
 # Create and implicit start container.
 then
-    echo 'Creating and starting mongo_zunka container:'
-    docker run -d --name mongo_zunka -v data:/data/db -p 27017:27017 mongo:3.6.3
-# Start mongo_zunka container.
+    echo 'Creating and starting zunka_mongo container:'
+    docker run -d --name zunka_mongo -v zunka_mongo_data:/data/db -p 27017:27017 mongo:3.6.3
+    echo '#################### No data, import backup to new created mongo docker ###########################!'
+# Start zunka_mongo container.
 else
-    echo 'Starting mongo_zunka container:'
-    docker start mongo_zunka
+    echo 'Starting zunka_mongo container:'
+    docker start zunka_mongo
 fi
 
 echo "To access mongo:"
-echo "  docker exec -it mongo_zunka bash"
+echo "  docker exec -it zunka_mongo bash"
 echo "  mongo zunka"
 
-exit
 
 
+# Create or start redis container.
+if [[ -z $(docker ps -a | grep redis_zunka) ]]
+# Create and implicit start container.
+then
+    echo 'Creating and starting redis_zunka container:'
+    # docker run -d --name zunka_mongo -v data:/data/db -p 27017:27017 mongo:3.6.3
+    docker run -d --name zunka_redis redis:4.0.9 redis-server --save 60 1 --loglevel warning
+    # docker run -d --name redis_zunka -v data_redis:/data redis:4.0.9 redis-server --save 60 1 --loglevel warning
+    # docker run -d --name redis_zunka redis redis-server --save 60 1 --loglevel warning
+    # docker run -d --name redis_zunka -v /docker/host/dir:/data redis redis-server --save 60 1 --loglevel warning
 
-# [[ `systemctl status mongodb | awk '/Active/{print $2}'` == inactive ]] && sudo systemctl start mongodb
+# Start zunka_redis container.
+else
+    echo 'Starting redis_zunka container:'
+    docker start zunka_redis
+fi
 
-# [[ `systemctl status redis | awk '/Active/{print $2}'` == inactive ]] && sudo systemctl start redis
-# [[ `systemctl status nginx | awk '/Active/{print $2}'` == inactive ]] && sudo systemctl start nginx
+echo "To access mongo:"
+echo "  docker exec -it zunka_redis sh"
+
+
+[[ `systemctl status nginx | awk '/Active/{print $2}'` == inactive ]] && sudo systemctl start nginx
 sleep .1
-
-echo 'Exiting'
-exit
-echo 'After exiting'
 
 # # Start freight server.
 # freightsrv &
